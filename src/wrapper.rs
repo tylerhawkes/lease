17876531f3f70ea80ce5989786516d095de5e2ec
Tyler Hawkes <tyler.hawkes@gmail.com>
2022-02-16 13:06:25 -0700

    Make wrapper implementations more straightforward

--- src/wrapper.rs
+++ src/wrapper.rs
@@ -20,17 +20,21 @@ impl<T> Wrapper<T> {
   pub(crate) fn new(t: T) -> Self {
     Self(Arc::new(Mutex::new(t)))
   }
+  #[inline]
+  fn as_usize(&self) -> usize {
+    Arc::as_ptr(&self.0) as usize
+  }
 }
 
 impl<T> core::hash::Hash for Wrapper<T> {
   fn hash<H: Hasher>(&self, state: &mut H) {
-    Arc::as_ptr(&self.0).hash(state);
+    self.as_usize().hash(state);
   }
 }
 
 impl<T> core::cmp::PartialEq for Wrapper<T> {
   fn eq(&self, other: &Self) -> bool {
-    Arc::as_ptr(&self.0) == Arc::as_ptr(&other.0)
+    self.as_usize() == other.as_usize()
   }
 }
 
@@ -38,12 +42,12 @@ impl<T> core::cmp::Eq for Wrapper<T> {}
 
 impl<T> core::cmp::PartialOrd for Wrapper<T> {
   fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-    Arc::as_ptr(&self.0).partial_cmp(&Arc::as_ptr(&other.0))
+    self.as_usize().partial_cmp(&other.as_usize())
   }
 }
 
 impl<T> core::cmp::Ord for Wrapper<T> {
   fn cmp(&self, other: &Self) -> Ordering {
-    Arc::as_ptr(&self.0).cmp(&Arc::as_ptr(&other.0))
+    self.as_usize().cmp(&other.as_usize())
   }
 }
